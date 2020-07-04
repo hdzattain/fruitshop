8507a29e05e1824963fa947ebf761ae695f89c29
hdzattain <58393005+hdzattain@users.noreply.github.com>
2020-07-04 14:05:35 +0800

    Update and rename t.cpp to App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/pear.jpg":style="{maxWidth:80+'px'}">
+    <img src="./assets/apple.jpg":style="{maxWidth:80+'px'}">
+    <img src="./assets/dragonfruit.jpg":style="{maxWidth:80+'px'}">
+    <img src="./assets/mango.jpg":style="{maxWidth:80+'px'}">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
